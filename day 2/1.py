0ed65c1d47f9619144dc50f295edf6e23f30f8db
JohnDev19 <137758073+JohnDev19@users.noreply.github.com>
2024-07-18 18:11:31 +0800

    Create 1.py

---
+++ day 2/1.py
@@ -0,0 +1,25 @@
+# Integer variables
+age = 30
+num_children = 2
+
+# Float variables
+height = 1.75
+weight = 68.5
+
+# String variables
+name = "John Doe"
+address = "123 Main St, Cityville"
+
+# Boolean variables
+is_student = False
+has_car = True
+
+# Printing out the variables...
+print("Name:", name)
+print("Age:", age)
+print("Address:", address)
+print("Height:", height, "m")
+print("Weight:", weight, "kg")
+print("Number of children:", num_children)
+print("Is student?", is_student)
+print("Has car?", has_car)
