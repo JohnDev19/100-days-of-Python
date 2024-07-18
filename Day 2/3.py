92278c4e554727a5dd3894759ceb2e77505656df
JohnDev19 <137758073+JohnDev19@users.noreply.github.com>
2024-07-18 18:30:12 +0800

    Create 3.py

---
+++ Day 2/3.py
@@ -0,0 +1,9 @@
+# String
+original_string = "Hello, World!"
+
+# Extracting a substring using slicing...
+substring = original_string[7:12]
+
+# Printing the original string and the extracted substring...
+print("Original String:", original_string)
+print("Extracted Substring:", substring)
