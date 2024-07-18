92075e5682b8eb90e8f140ded4e7e8fcb167a77e
JohnDev19 <137758073+JohnDev19@users.noreply.github.com>
2024-07-18 18:16:31 +0800

    Create 4.py

---
+++ day 2/4.py
@@ -0,0 +1,13 @@
+# String
+my_string = "Hello, World!"
+
+# Converting the string to uppercase...
+uppercase_string = my_string.upper()
+
+# Converting the string to lowercase...
+lowercase_string = my_string.lower()
+
+# Printing the original string, uppercase string, and lowercase string...
+print("Original String:", my_string)
+print("Uppercase String:", uppercase_string)
+print("Lowercase String:", lowercase_string)
