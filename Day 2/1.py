8e76640b2ff25defbdd0226f70a0d88ebf1b2fd9
JohnDev19 <137758073+JohnDev19@users.noreply.github.com>
2024-07-18 18:29:35 +0800

    Create 1.py

---
+++ Day 2/1.py
@@ -0,0 +1,9 @@
+def concatenate_strings(str1, str2):
+    concatenated = f"{str1}{str2}"
+    return concatenated
+
+# Usage
+string1 = "Hello, "
+string2 = "World!"
+result = concatenate_strings(string1, string2)
+print(result)  # Output: Hello, World!
