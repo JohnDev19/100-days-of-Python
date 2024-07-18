6426126d5d9b67ce6827d669f4ca499d9dfbaf9d
JohnDev19 <137758073+JohnDev19@users.noreply.github.com>
2024-07-18 18:27:35 +0800

    Update 1.py

--- day 2/1.py
+++ day 2/1.py
@@ -1,25 +1,8 @@
-# Integer variables
-age = 30
-num_children = 2
+def concatenate_strings(str1, str2):
+    concatenated = f"{str1}{str2}"
+    return concatenated
 
-# Float variables
-height = 1.75
-weight = 68.5
-
-# String variables
-name = "John Doe"
-address = "123 Main St, Cityville"
-
-# Boolean variables
-is_student = False
-has_car = True
-
-# Printing out the variables...
-print("Name:", name)
-print("Age:", age)
-print("Address:", address)
-print("Height:", height, "m")
-print("Weight:", weight, "kg")
-print("Number of children:", num_children)
-print("Is student?", is_student)
-print("Has car?", has_car)
+string1 = "Hello, "
+string2 = "World!"
+result = concatenate_strings(string1, string2)
+print(result) 
