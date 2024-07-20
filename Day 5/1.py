d61ebdfbe76416206135acb090e955311d8d2285
JohnDev19 <137758073+JohnDev19@users.noreply.github.com>
2024-07-21 07:53:50 +0800

    Create 1.py

---
+++ Day 5/1.py
@@ -0,0 +1,7 @@
+# Tuple / data types
+mixed_tuple = (42, 3.14, "Hello, World!", True, [1, 2, 3])
+
+# Printing the elements of the tuple...
+print("Elements of the tuple with various data types:")
+for element in mixed_tuple:
+    print(element)
