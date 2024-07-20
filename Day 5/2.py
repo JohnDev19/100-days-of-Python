b652f5e5971f808574ed69aaedba9eeb019351a7
JohnDev19 <137758073+JohnDev19@users.noreply.github.com>
2024-07-21 07:54:26 +0800

    Create 2.py

---
+++ Day 5/2.py
@@ -0,0 +1,13 @@
+# Tuple
+fruit_tuple = ('apple', 'banana', 'cherry', 'date', 'elderberry')
+
+# Accessing elements using positive and negative indexing...
+first_element = fruit_tuple[0]
+third_element = fruit_tuple[2]
+last_element = fruit_tuple[-1]
+second_last_element = fruit_tuple[-2]
+
+print(f"First element: {first_element}")
+print(f"Third element: {third_element}")
+print(f"Last element: {last_element}")
+print(f"Second last element: {second_last_element}")
