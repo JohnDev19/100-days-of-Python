93a803351aa652a786702ed7ac46384ea4d3122a
JohnDev19 <137758073+JohnDev19@users.noreply.github.com>
2024-07-21 07:55:52 +0800

    Create 4.py

---
+++ Day 5/4.py
@@ -0,0 +1,6 @@
+# A tuple with complex elements
+complex_tuple = (5, 10, (15, 20), [25, 30], 'length')
+
+# Finding the length of the tuple...
+tuple_length = len(complex_tuple)
+print(f"Length of the complex tuple: {tuple_length}")
