10e80b0629fc4190995b280defe9e88c886bd6de
JohnDev19 <137758073+JohnDev19@users.noreply.github.com>
2024-07-24 06:53:07 +0800

    Create 5.py

---
+++ Day 6/5.py
@@ -0,0 +1,8 @@
+# Set
+my_set = {10, 20, 30, 40, 50}
+
+# Finding the length of the set...
+set_length = len(my_set)
+
+# Printing the length of the set...
+print("The length of the set is:", set_length)
