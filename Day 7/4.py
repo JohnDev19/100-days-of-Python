c5fcf364f1255fee1dba5c71f9a334b5e89b7732
JohnDev19 <137758073+JohnDev19@users.noreply.github.com>
2024-07-24 06:58:09 +0800

    Create 4.py

---
+++ Day 7/4.py
@@ -0,0 +1,13 @@
+# Dictionary
+my_dict = {
+    "name": "Alice",
+    "age": 30,
+    "city": "New York",
+    "occupation": "Engineer"
+}
+
+# Finding the length of the dictionary...
+dict_length = len(my_dict)
+
+# Printing the length of the dictionary...
+print("The length of the dictionary is:", dict_length)
