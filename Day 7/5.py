1f415cc13da860875cf7f0eefe8fc3f221b91819
JohnDev19 <137758073+JohnDev19@users.noreply.github.com>
2024-07-24 06:59:06 +0800

    Create 5.py

---
+++ Day 7/5.py
@@ -0,0 +1,22 @@
+# Dictionary
+my_dict = {
+    "name": "Alice",
+    "age": 30,
+    "city": "New York",
+    "occupation": "Engineer"
+}
+
+# Iterating through dictionary keys...
+print("Iterating through keys:")
+for key in my_dict:
+    print(f"Key: {key}")
+
+# Iterating through dictionary values...
+print("\nIterating through values:")
+for value in my_dict.values():
+    print(f"Value: {value}")
+
+# Iterating through dictionary key-value pairs...
+print("\nIterating through key-value pairs:")
+for key, value in my_dict.items():
+    print(f"Key: {key}, Value: {value}")
