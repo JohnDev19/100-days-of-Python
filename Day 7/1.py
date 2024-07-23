bf4ef22acc8ac231578768a21c1cecd0d443760b
JohnDev19 <137758073+JohnDev19@users.noreply.github.com>
2024-07-24 06:55:07 +0800

    Create 1.py

---
+++ Day 7/1.py
@@ -0,0 +1,10 @@
+# Dictionary
+my_dict = {
+    "name": "Alice",
+    "age": 30,
+    "city": "New York"
+}
+
+# Printing the elements of the dictionary...
+for key, value in my_dict.items():
+    print(f"Key: {key}, Value: {value}")
